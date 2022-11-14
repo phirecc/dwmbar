0a38e2c617d425658e17520440ec6b99fc105f6f
phire <me@phire.cc>
2022-11-14 13:20:55 +0100

    add mlvd to vpns

--- src/modules/vpn.rs
+++ src/modules/vpn.rs
@@ -4,7 +4,7 @@ pub struct Vpn {
 }
 impl Module for Vpn {
     fn eval(&self) -> ModuleResult {
-        let vpns = ["wg0", "wgnord", "wg-mullvad"];
+        let vpns = ["wg0", "wgnord", "wg-mullvad", "mlvd"];
         for vpn in vpns {
             if Path::new(&("/sys/class/net/".to_owned() + vpn)).exists() {
                 return Ok(Some("VPN".to_string()));
